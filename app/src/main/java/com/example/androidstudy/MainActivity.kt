8d02fab960aa14ce7daaca69f4651f8481004954
Doyun <materokatti@gmail.com>
2024-03-11 13:31:22 +0900

    ADD : onButtonClick (MainActivity.kt)

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.androidstudy
 
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClick(view: View) {
+        val text1 = findViewById<TextView>(R.id.text1)
+        when (view.id) {
+            R.id.button1 -> text1.text = "hello"
+            R.id.button2 -> text1.text = "world"
+        }
     }
 }
